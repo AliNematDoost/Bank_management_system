fd6fb47bbd621db898b47052569f99400497b01e
AliND <alinematdoost82@gmail.com>
2025-09-21 14:33:22 +0330

    Initial commit

---
+++ src/main/java/com/example/mohaimen/MohaimenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mohaimen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohaimenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MohaimenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mohaimen/MohaimenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mohaimen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohaimenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
