7f3259ddbbcc5cc1467c6a38214552b82747ea62
AliND <alinematdoost82@gmail.com>
2025-09-22 19:27:55 +0330

    fifth Req -- bugs fix

--- src/main/java/com/example/mohaimen/Controller/TransactionController.java
+++ src/main/java/com/example/mohaimen/Controller/TransactionController.java
@@ -27,7 +27,7 @@ public class TransactionController {
 
     // DEPOSIT AMOUNT TO AN ACCOUNT
     @PostMapping("")
-    public ResponseEntity<Transaction> depositOrWithdraw(@RequestBody DepositTran depositTran) {
+    public ResponseEntity<?> depositOrWithdraw(@RequestBody DepositTran depositTran) {
         BigDecimal amount = depositTran.getAmount();
         String accountNumber = depositTran.getAccountNumber();
 
@@ -35,14 +35,20 @@ public class TransactionController {
 
         // account not found
         if (optAccount.isEmpty()) {
-            return ResponseEntity.notFound().build();
+            Transaction transaction = new Transaction(depositTran.getTransactionType(), amount, new Date());
+            transaction.setStatus(Status.FAILED);
+            transactionRepository.save(transaction);
+            return ResponseEntity.badRequest().body("Account not found!");
         }
 
         Account account = optAccount.get();
 
         // transaction type not valid
         if (depositTran.getTransactionType() == null) {
-            return ResponseEntity.badRequest().build();
+            Transaction transaction = new Transaction(TransactionType.INVALID, amount, new Date());
+            transaction.setStatus(Status.FAILED);
+            transactionRepository.save(transaction);
+            return ResponseEntity.badRequest().body("Transaction type not valid!");
         }
 
         if (depositTran.getTransactionType().equals(TransactionType.DEPOSIT)) {
@@ -55,7 +61,12 @@ public class TransactionController {
             return ResponseEntity.ok(transaction1);
         } else if (depositTran.getTransactionType().equals(TransactionType.WITHDRAW)) {
             if (account.getBalance().compareTo(amount.add(transactionFee)) < 0) {
-                return ResponseEntity.badRequest().body(null);
+                Transaction transaction = new Transaction(TransactionType.WITHDRAW,
+                        depositTran.getAmount(),
+                        new Date());
+                transaction.setStatus(Status.FAILED);
+                transactionRepository.save(transaction);
+                return ResponseEntity.badRequest().body("Not enough balance in account!");
             }
 
             account.setBalance(account.getBalance().subtract(amount).subtract(transactionFee));
@@ -72,19 +83,29 @@ public class TransactionController {
 
     // TRANSFER AMOUNT FROM ONE ACCOUNT TO ANOTHER
     @PostMapping("/transfer")
-    public ResponseEntity<Transaction> transferAmount(@RequestBody Transfer transfer) {
+    public ResponseEntity<?> transferAmount(@RequestBody Transfer transfer) {
 
         // check if accounts exist
         Optional<Account> optFromAccount = accountRepository.findByAccountNumber(transfer.getFromAccount());
         Optional<Account> optToAccount = accountRepository.findByAccountNumber(transfer.getToAccount());
 
         if (optFromAccount.isEmpty() || optToAccount.isEmpty()) {
-            return ResponseEntity.notFound().build();
+            Transaction transaction = new Transaction(TransactionType.TRANSFER,
+                    transfer.getAmount(),
+                    new Date());
+            transaction.setStatus(Status.FAILED);
+            transactionRepository.save(transaction);
+            return ResponseEntity.badRequest().body("One or both accounts not found!");
         }
 
         // check if fromAccount and toAccount are different
         if (transfer.getFromAccount().equals(transfer.getToAccount())) {
-            return ResponseEntity.badRequest().build();
+            Transaction transaction = new Transaction(TransactionType.TRANSFER,
+                    transfer.getAmount(),
+                    new Date());
+            transaction.setStatus(Status.FAILED);
+            transactionRepository.save(transaction);
+            return ResponseEntity.badRequest().body("From and To account cannot be the same!");
         }
 
         // check if fromAccount and toAccount are active
@@ -92,13 +113,23 @@ public class TransactionController {
         Account toAccount = optToAccount.get();
         if (fromAccount.getAccountStatus().equals(AccountStatus.INACTIVE) ||
                 toAccount.getAccountStatus().equals(AccountStatus.INACTIVE)) {
-            return ResponseEntity.badRequest().build();
+            Transaction transaction = new Transaction(TransactionType.TRANSFER,
+                    transfer.getAmount(),
+                    new Date());
+            transaction.setStatus(Status.FAILED);
+            transactionRepository.save(transaction);
+            return ResponseEntity.badRequest().body("From and To account must be active!");
         }
 
         // check if fromAccount has sufficient balance
         BigDecimal amount = transfer.getAmount();
         if (fromAccount.getBalance().compareTo(amount.add(transactionFee)) < 0) {
-            return ResponseEntity.badRequest().body(null);
+            Transaction transaction = new Transaction(TransactionType.TRANSFER,
+                    transfer.getAmount(),
+                    new Date());
+            transaction.setStatus(Status.FAILED);
+            transactionRepository.save(transaction);
+            return ResponseEntity.badRequest().body("Not enough balance in from account!");
         }
 
         // transfer amount
@@ -112,4 +143,6 @@ public class TransactionController {
         transactionRepository.save(transaction);
         return ResponseEntity.ok(transaction);
     }
+
+
 }
--- src/main/java/com/example/mohaimen/model/TransactionType.java
+++ src/main/java/com/example/mohaimen/model/TransactionType.java
@@ -3,5 +3,6 @@ package com.example.mohaimen.model;
 public enum TransactionType {
     DEPOSIT,
     TRANSFER,
-    WITHDRAW
+    WITHDRAW,
+    INVALID
 }
