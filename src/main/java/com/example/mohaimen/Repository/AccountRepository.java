ca1dc3ce9e5ffc5f2c809490512f02492054fcdd
AliND <alinematdoost82@gmail.com>
2025-09-24 18:30:49 +0330

    Add pessimistic locking to account

--- src/main/java/com/example/mohaimen/Repository/AccountRepository.java
+++ src/main/java/com/example/mohaimen/Repository/AccountRepository.java
@@ -1,7 +1,11 @@
 package com.example.mohaimen.Repository;
 
 import com.example.mohaimen.model.Account;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +16,7 @@ public interface AccountRepository extends JpaRepository<Account, String> {
     boolean existsAccountByNationalId(String nationalId);
     boolean existsAccountByAccountNumber(String accountNumber);
     List<Account> findAllByAccountNumber(String accountNumber);
-    Optional<Account> findByAccountNumber(String accountNumber);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT a FROM Account a WHERE a.accountNumber = :accountNumber")
+    Optional<Account> findByAccountNumber(@Param("accountNumber") String accountNumber);
 }
